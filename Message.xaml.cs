cfb7e63e1624d61b1edfb85603e832a82a0fa33c
Cirilla <gazlayxyc@outlook.com>
2021-11-13 18:15:33 +0800

    working on download

--- Message.xaml.cs
+++ Message.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +24,11 @@ namespace CyDrive
         {
             InitializeComponent();
         }
+        private async void Send_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Send Clicked");
+            
+        }
     }
+    
 }
